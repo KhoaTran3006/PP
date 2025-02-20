bf8fc755564647d533fcd66d3c077ea864968e99
cuong1010 <manhcuongit1010@gmail.com>
2025-02-20 12:16:55 +0700

    demo script

---
+++ Assets/Script/PaintingBoard/Picture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PaintingBoard
+{
+    public class Picture : MonoBehaviour, IPickable
+    {
+        public GameObject GameObject => throw new System.NotImplementedException();
+
+        public void Drop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Pickup()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
